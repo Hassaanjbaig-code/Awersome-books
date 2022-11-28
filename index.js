bb5f0f3dab35a6f2910aeb89c15082218101a3e8
Allassane <dickoallassane1997@gmail.com>
2022-11-28 12:30:28 +0100

    Add remove book feature

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 let input = document.querySelector('.form');
-let Title = document.getElementById('Title');
-let Author = document.getElementById('Author');
 let button = document.getElementById('button');
+let listBooks = document.querySelector('.list-book');
+listBooks.style.listStyle = "none"
 
-
-let show = document.querySelector('.list-book');
+let currendID = 0;
 
 let collection = [];
+
+function deleteBook(id){
+  document.getElementById(id).remove()
+}
+
 function insert() {
-    let newtitle = Title.innertext
-    let nameauthor = Author.innertext
-    collection.push({title: newtitle, author: nameauthor});
-    console.log(collection);
+  var Title = document.getElementById('Title').value;
+  var Author = document.getElementById('Author').value;
+  collection.push({title: Title, author: Author});
+  console.log(collection);
+
+  //Display the book dynamically
+  let li = document.createElement('li')
+  li.id = "book-"+currendID
+  let pTitle = document.createElement("p")
+  pTitle.textContent = Title
+  let pAuthor = document.createElement('p')
+  pAuthor.textContent = Author
+  let btnRemove = document.createElement('button')
+  btnRemove.textContent = "Remove"
+
+  var ItemId = "book-"+currendID
+  btnRemove.addEventListener('click', () => {
+    deleteBook(ItemId)
+  })
+
+  let hr = document.createElement('hr')
+
+  li.append(pTitle, pAuthor, btnRemove, hr)
+
+  listBooks.append(li)
+  currendID = currendID + 1
 }
 
 button.addEventListener("click", insert)
