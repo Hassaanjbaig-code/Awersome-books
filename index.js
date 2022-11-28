e1c02250c5b1e53aac668a59c6296610a2eda41a
Allassane <dickoallassane1997@gmail.com>
2022-11-28 13:53:54 +0100

    Update deleteBook function: fix bug

--- index.js
+++ index.js
@@ -1,55 +1,58 @@
-let input = document.querySelector('.form');
-let button = document.getElementById('button');
-let listBooks = document.querySelector('.list-book');
-listBooks.style.listStyle = "none"
+const button = document.getElementById('button');
+const listBooks = document.querySelector('.list-book');
+listBooks.style.listStyle = 'none';
 
 let currendID = 0;
 
 let collection = [];
 
-function deleteBook(id){
-  document.getElementById(id).remove()
+function deleteBook(id) {
+  document.getElementById(id).remove();
+
+  // id = index for collection list
+  collection = collection.filter((book) => book.id !== id);
 }
 
-function insert(Title , Author) {
-    collection.push({title: Title, author: Author});
-    console.log(collection);
-    
-    //Display the book dynamically
-    let li = document.createElement('li')
-    li.id = "book-"+currendID
-    let pTitle = document.createElement("p")
-  pTitle.textContent = Title
-  let pAuthor = document.createElement('p')
-  pAuthor.textContent = Author
-  let btnRemove = document.createElement('button')
-  btnRemove.textContent = "Remove"
+function insert(Title, Author) {
+  const itemId = `book-${currendID}`;
+  collection.push({ id: itemId, title: Title, author: Author });
 
-  var ItemId = "book-"+currendID
+  // Display the book dynamically
+  const li = document.createElement('li');
+  li.id = itemId;
+  const pTitle = document.createElement('p');
+  pTitle.textContent = Title;
+  const pAuthor = document.createElement('p');
+  pAuthor.textContent = Author;
+  const btnRemove = document.createElement('button');
+  btnRemove.textContent = 'Remove';
+
   btnRemove.addEventListener('click', () => {
-    deleteBook(ItemId)
-})
+    deleteBook(itemId);
+  });
 
-let hr = document.createElement('hr')
+  const hr = document.createElement('hr');
 
-  li.append(pTitle, pAuthor, btnRemove, hr)
-  
-  listBooks.append(li)
-  currendID = currendID + 1
+  li.append(pTitle, pAuthor, btnRemove, hr);
+
+  listBooks.append(li);
+  currendID += 1;
 }
 
-//  Add the book in the local storage 
+//  Add the book in the local storage
 
-window.addEventListener("beforeunload", () => {
-    window.localStorage.setItem("Collection", JSON.stringify(collection))
-})
-let list = JSON.parse(window.localStorage.getItem("Collection"));
-list.forEach((books) => {
-    insert(books.title, books.author)
-    console.log(books)
+window.addEventListener('beforeunload', () => {
+  window.localStorage.setItem('Collection', JSON.stringify(collection));
 });
-button.addEventListener("click", () => {
-    var Title = document.getElementById('Title').value;
-    var Author = document.getElementById('Author').value;
-    insert(Title, Author)
-})
+const list = JSON.parse(window.localStorage.getItem('Collection'));
+if (list !== null) {
+  list.forEach((books) => {
+    insert(books.title, books.author);
+  });
+}
+
+button.addEventListener('click', () => {
+  const Title = document.getElementById('Title').value;
+  const Author = document.getElementById('Author').value;
+  insert(Title, Author);
+});
