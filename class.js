94ba4074d0f52b6e9a9cfc1ff4792d9e64a60c39
Allassane <dickoallassane1997@gmail.com>
2022-11-29 12:32:56 +0100

    Update class.js: Add local storage, add remove book feature

--- class.js
+++ class.js
@@ -1,51 +1,67 @@
-class datastructure{
-    constructor(title,author){
-        this.Title = title,
-        this.Author = author
+let currendID = 0
+class Book{
+    constructor(id, title,author){
+      this.id = id
+      this.title = title,
+      this.author = author
     }
 }
 
-class dataintake{
+class ListBooks{
     constructor(){
-        this.bookcollection = []
+      this.bookcollection = []
     }
-    added(title,author){
-        let collection = [];
-        
-        var book = new datastructure(title,author);
-        collection.push(book);
-        structure(title,author)
-        console.log(collection)
+    add(title,author){
+      var book = new Book(currendID, title, author);
+      this.bookcollection.push(book);
+      structure(title,author)
+    }
+    delete(id){
+      document.getElementById(id).remove();
+      this.bookcollection = this.bookcollection.filter((book) => book.id !== id);
     }
 }
 
 
 function structure(title,author){
+    const itemId = currendID;
+
     const listBooks = document.querySelector('.list-book');
     const li = document.createElement('li');
-    // li.id = itemId;
-    const pTitle = document.createElement('p');
-    pTitle.textContent = title;
-    const pAuthor = document.createElement('p');
-    pAuthor.textContent = author;
+    li.id = currendID;
+    const pTitleAuthor = document.createElement('p');
+    pTitleAuthor.textContent = `"${title}" by ${author}`;
+    
     const btnRemove = document.createElement('button');
     btnRemove.textContent = 'Remove';
     
-    // btnRemove.addEventListener('click', () => {
-        //   deleteBook(itemId);
-        // });
+    btnRemove.addEventListener('click', () => {
+      listBook.delete(itemId)
+    });
         
-        const hr = document.createElement('hr');
+    li.append(pTitleAuthor, btnRemove);
         
-        li.append(pTitle, pAuthor, btnRemove, hr);
-        
-        listBooks.append(li);
-        // currendID += 1;
-    }
-    var button = document.querySelector("#button");
-    button.addEventListener("click", () => {
-        var title = document.getElementById("Title").value;
-        var author = document.getElementById("Author").value;
-    var active = new dataintake()
-    active.added(title,author)
+    listBooks.append(li);
+    currendID += 1;
+}
+
+var button = document.querySelector("#button");
+
+var listBook = new ListBooks()
+button.addEventListener("click", () => {
+  var title = document.getElementById("Title").value;
+  var author = document.getElementById("Author").value;
+  listBook.add(title, author)
 })
+
+//  Add the book in the local storage
+window.addEventListener('beforeunload', () => {
+  localStorage.setItem('Collection', JSON.stringify(listBook.bookcollection));
+});
+
+if (window.localStorage.getItem('Collection') !== "undefined") {
+  const list = JSON.parse(window.localStorage.getItem('Collection'));
+  list.forEach((books) => {
+    listBook.add(books.title, books.author);
+  });
+}
